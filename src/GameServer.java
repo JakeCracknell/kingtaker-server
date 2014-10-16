1a66ba7af49503d1395490056b60cd04a3baf80f
Jake Cracknell <jc4512@ray06.doc.ic.ac.uk>
2014-10-16 12:30:40 +0100

    made GameServer class implement runnable

---
+++ src/GameServer.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jc4512 on 16/10/14.
+ */
+public class GameServer implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("GameServer.run");
+        }
+    }
+}
--- src/frmMain.java
+++ src/frmMain.java
@@ -7,11 +7,14 @@ public class frmMain {
 
     private JTable table1;
     private JPanel panel1;
+    private JButton button1;
 
     public frmMain() {
 
     }
 
+    //Boilerplate code to initialise and display GUI form.
+    //Starting listening server
     public static void main(String[] args) {
         JFrame frame = new JFrame("frmMain");
         frame.setContentPane(new frmMain().panel1);
@@ -19,5 +22,9 @@ public class frmMain {
         frame.pack();
         frame.setVisible(true);
         frame.setResizable(false);
+
+        GameServer gameServer = new GameServer();
+        Thread gameServerThread = new Thread(gameServer);
+        gameServerThread.start();
     }
 }
