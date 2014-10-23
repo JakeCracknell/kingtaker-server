9ca109fe37b1028270b332bc858054f13bd8a074
Jake Cracknell <jc4512@ray20.doc.ic.ac.uk>
2014-10-23 15:35:13 +0100

    specified protocol for kingtaker c-c c-s s-s communication

---
+++ src/NetworkingCodes/ClientCommandCode.java
@@ -0,0 +1,12 @@
+package NetworkingCodes;
+
+/**
+ * Created by jc4512 on 23/10/14.
+ */
+public enum ClientCommandCode {
+    GET_GAME_LIST,
+    AUTHENTICATE_USER,
+    CREATE_GAME,
+    REMOVE_GAME,
+    REPORT_PLAYER
+}
---
+++ src/NetworkingCodes/ResponseCode.java
@@ -0,0 +1,7 @@
+package NetworkingCodes;
+
+/**
+ * Created by jc4512 on 23/10/14.
+ */
+public enum ResponseCode {
+}
