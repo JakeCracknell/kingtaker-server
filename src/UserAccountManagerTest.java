fb0fb1efe29352e7c64acad5a509020a10b796fc
Jake Cracknell <jc4512@corona01.doc.ic.ac.uk>
2014-10-24 16:47:00 +0100

    all UAM tests written and pass :)

--- src/UserAccountManagerTest.java
+++ src/UserAccountManagerTest.java
@@ -7,23 +7,26 @@ import static org.junit.Assert.*;
 
 public class UserAccountManagerTest {
 
-    //input into database
+    //all testUser credentials are input into database (many tests fail otherwise)
     private final String testUserNameInDB = "***TESTUSER***";
     private final String testUserNameNotInDB = "***NOTATESTUSER***";
     private final int testPasswordHash = 0;
-    private InetAddress testIP;
+    private final GameUser testUserRogue = new GameUser("bob",1900);
+    private InetAddress testIP1;
+    private InetAddress testIP2;
     private UserAccountManager uam;
 
     //all tests will fail if no database connection
     @Before
     public void setUp() throws Exception {
         uam = new UserAccountManager();
-        testIP = InetAddress.getByAddress(new byte[]{8,8,8,8});
+        testIP1 = InetAddress.getByAddress(new byte[]{8,8,8,8});
+        testIP2 = InetAddress.getByAddress(new byte[]{8,8,4,4});
     }
 
     @Test
     public void testAuthenticateUserOK() throws Exception {
-        GameUser gameUser = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP);
+        GameUser gameUser = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP1);
         assertNotNull(gameUser);
 
         assertTrue(uam.checkUserIsAuthenticated(gameUser));
@@ -31,15 +34,13 @@ public class UserAccountManagerTest {
 
     @Test
     public void testAuthenticateUserTwice() throws Exception {
-        InetAddress testIP2 = InetAddress.getByAddress(new byte[]{8,8,4,4});
-
         GameUser gameUser1 = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP2);
-        GameUser gameUser2 = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP);
+        GameUser gameUser2 = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP1);
 
         //gameUser1 == gameUser2
         assertTrue(uam.checkUserIsAuthenticated(gameUser2));
 
-        GameUser gameUser3 = uam.getUserByAddress(testIP);
+        GameUser gameUser3 = uam.getUserByAddress(testIP1);
         assertNotNull(gameUser3);
         GameUser gameUser4 = uam.getUserByAddress(testIP2);
         assertNull(gameUser4);
@@ -47,7 +48,7 @@ public class UserAccountManagerTest {
 
     @Test
     public void testAuthenticateUserBadName() throws Exception {
-        GameUser gameUser = uam.authenticateUser(testUserNameNotInDB, testPasswordHash, testIP);
+        GameUser gameUser = uam.authenticateUser(testUserNameNotInDB, testPasswordHash, testIP1);
         assertNull(gameUser);
 
         assertFalse(uam.checkUserIsAuthenticated(gameUser));
@@ -55,25 +56,48 @@ public class UserAccountManagerTest {
 
     @Test
     public void testAuthenticateUserBadPassword() throws Exception {
-        GameUser gameUser = uam.authenticateUser(testUserNameNotInDB, 1, testIP);
+        GameUser gameUser = uam.authenticateUser(testUserNameNotInDB, 1, testIP1);
         assertNull(gameUser);
 
         assertFalse(uam.checkUserIsAuthenticated(gameUser));
     }
 
     @Test
-    public void testGetUserByAddress() throws Exception {
+    public void testGetUserByAddressPositive() throws Exception {
+        GameUser gameUser1 = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP1);
+        GameUser gameUser2 = uam.getUserByAddress(testIP1);
+        assertEquals(gameUser1, gameUser2);
+    }
 
+    @Test
+    public void testGetUserByAddressNegative() throws Exception {
+        uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP1);
+        GameUser gameUser2 = uam.getUserByAddress(testIP2);
+        assertNull(gameUser2);
     }
 
     @Test
     public void testCheckUserIsAuthenticatedNegative() throws Exception {
         UserAccountManager uam = new UserAccountManager();
-        assertFalse(uam.checkUserIsAuthenticated(new GameUser("bob",1900)));
+        assertFalse(uam.checkUserIsAuthenticated(testUserRogue));
     }
 
     @Test
-    public void testUnauthenticateUser() throws Exception {
+    public void testUnauthenticateUserPositive() throws Exception {
+        GameUser gameUser = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP1);
 
+        uam.unauthenticateUser(gameUser);
+
+        assertNull(uam.getUserByAddress(testIP1));
+        assertFalse(uam.checkUserIsAuthenticated(gameUser));
+    }
+
+    @Test
+    public void testUnauthenticateUserNegative() throws Exception {
+        GameUser gameUser = uam.authenticateUser(testUserNameInDB, testPasswordHash, testIP1);
+
+        uam.unauthenticateUser(testUserRogue);
+
+        assertTrue(uam.checkUserIsAuthenticated(gameUser));
     }
 }
